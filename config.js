5e41ea6de403eaa732b61fa3b0a13c58352e35f6
PrinceBot-Ratthanin <50706633+PrinceBot-Ratthanin@users.noreply.github.com>
2023-09-28 00:51:52 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {    
+    name : 'NKP_V2',
+    platform : 'arduino-esp32',
+    title : 'NKP_V2',
+    description : 'บอร์ดสำหรับการเรียนรู้การควบคุมหุ่นยนต์เบื้องต้น',
+    author : 'PrinceBot',
+    website : 'http://www.shop.princebot.co.th/',
+    email : 'print081@gmail.com',
+    git : 'https://github.com/PrinceBot-Ratthanin/NKP_ONE-KB-IDE/',    
+    image : '/static/NKP_V2.jpg',
+    version : '0.1'
+};
---
+++ template.c
@@ -0,0 +1,25 @@
+#include "NKP_V2.h"
+
+
+${EXTINC}
+
+
+${VARIABLE}
+
+${FUNCTION}
+
+void setup()
+{
+	
+	NKP_V2();
+    /* setup code */
+${SETUP_CODE}
+    /* block setup */
+${BLOCKSETUP}
+}
+
+void loop()
+{
+  ${LOOP_CODE}
+  ${LOOP_EXT_CODE}
+}
